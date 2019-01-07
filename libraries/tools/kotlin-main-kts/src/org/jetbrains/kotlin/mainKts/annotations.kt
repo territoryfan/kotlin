d7c7df6bb95cd8e934ec4667be4fea1fdde85c9e
Ilya Chernikov <ilya.chernikov@jetbrains.com>
2019-01-07 18:11:43 +0100

    Add import support to main-kts, with test

---
+++ libraries/tools/kotlin-main-kts/src/org/jetbrains/kotlin/mainKts/annotations.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.mainKts
+
+/**
+ * Import other script(s)
+ */
+@Target(AnnotationTarget.FILE)
+@Repeatable
+@Retention(AnnotationRetention.SOURCE)
+annotation class Import(vararg val sources: String)
+
